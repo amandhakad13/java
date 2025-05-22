3396b10277d307be898d5bd3000b15eb55795d12
amandhakad13 <ad5538694@gmail.com>
2025-05-22 12:43:55 -0700

    string to char arr

---
+++ string programs/Test3.java
@@ -0,0 +1,12 @@
+class Test3 {
+	public static void main(String[] args) {
+		String s = "Welcome";
+		
+		char[] ch = s.toCharArray();
+		
+		for(char c : ch) {
+			System.out.println(c);
+		}
+		
+	}
+}
