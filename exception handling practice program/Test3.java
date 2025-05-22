8c37ec0ec340ca6b43ed12521a14596668456c33
amandhakad13 <ad5538694@gmail.com>
2025-05-22 16:43:30 -0700

    finally block program

---
+++ exception handling practice program/Test3.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class Test3 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter number 1 : ");
+		int num1 = sc.nextInt();
+		System.out.print("Enter number 2 : ");
+		int num2 = sc.nextInt();
+		
+		try {
+			int div = num1/num2;
+			System.out.println(num1+" / "+num2+" = "+div);
+		}
+		catch(ArithmeticException e) {
+			System.out.println(e);
+		}
+		finally {
+			System.out.println("Program end");
+		}
+	}
+}
