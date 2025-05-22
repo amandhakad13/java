9b625d13648a7c5bd04bc3a8efaa866972d052dd
amandhakad13 <ad5538694@gmail.com>
2025-05-22 12:55:55 -0700

    string equal or not program

---
+++ string programs/Test6.java
@@ -0,0 +1,11 @@
+class Test6 {
+	public static void main(String[] args) {
+		String s1 = new String("Programming");
+		String s2 = new String("Programming");
+		
+		boolean isequal = s1.equals(s2);
+		
+		System.out.println(isequal);
+		System.out.println(s1==s2);
+	}
+}
