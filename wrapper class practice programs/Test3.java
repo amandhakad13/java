85ce11c7bca03a2f827e5b1ca36be76784652989
amandhakad13 <ad5538694@gmail.com>
2025-05-24 11:36:03 -0700

    decimal to binary program

---
+++ wrapper class practice programs/Test3.java
@@ -0,0 +1,12 @@
+public class Test3 {
+	
+	public static void main(String[] args) {
+		
+		int num = 28;
+		
+		String bin = Integer.toBinaryString(num);
+		
+		System.out.println("Binary of "+num+" is : "+bin);
+		
+	}
+}
