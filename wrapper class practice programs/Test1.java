3c7ef3b36bd98872dc2ed88da27f7b3c7c50e7cb
amandhakad13 <ad5538694@gmail.com>
2025-05-24 11:27:51 -0700

    decimal to hexadecimal program

---
+++ wrapper class practice programs/Test1.java
@@ -0,0 +1,12 @@
+public class Test1 {
+	
+	public static void main(String[] args) {
+		
+		int num = 28;
+		
+		String hex = Integer.toHexString(num);
+		
+		System.out.println("Hexadecimal of "+num+" is : "+hex);
+		
+	}
+}
