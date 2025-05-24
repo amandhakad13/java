c8460d6e1efd7ce82e0eb38e979dc8dc23de3932
amandhakad13 <ad5538694@gmail.com>
2025-05-24 11:29:31 -0700

    decimal to octal program

---
+++ wrapper class practice programs/Test2.java
@@ -0,0 +1,12 @@
+public class Test2 {
+	
+	public static void main(String[] args) {
+		
+		int num = 28;
+		
+		String hex = Integer.toOctalString(num);
+		
+		System.out.println("Hexadecimal of "+num+" is : "+hex);
+		
+	}
+}
