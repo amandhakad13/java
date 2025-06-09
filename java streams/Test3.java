9a03f6328a1e3ebfcebfaa600e75a8c15deb70fe
amandhakad13 <ad5538694@gmail.com>
2025-06-09 16:38:35 -0700

    count digits, spaces, letters and special characters program

--- java streams/Test3.java
+++ java streams/Test3.java
@@ -2,7 +2,7 @@ import java.io.*;
 
 class Test3 {
 	public static void main(String[] args) {
-		int sCount = 0, scCount = 0, charcter = 0, digitCount = 0;
+		int sCount = 0, scCount = 0, lettersCount = 0, digitCount = 0;
 		FileInputStream fis = null;
 		
 		try {
@@ -15,7 +15,7 @@ class Test3 {
 					sCount++;
 				}
 				else if((b[i]>=65&&b[i]<=90)||(b[i]>=97&&b[i]<=122)) {
-					charcter++;
+					lettersCount++;
 				}
 				else if(b[i]>=48&&b[i]<=57) {
 					digitCount++;
@@ -25,7 +25,7 @@ class Test3 {
 				}
 			}
 			System.out.println("Spaces = "+sCount);
-			System.out.println("Characters = "+charcter);
+			System.out.println("Letters = "+lettersCount);
 			System.out.println("digits = "+digitCount);
 			System.out.println("Special Characters = "+scCount);
 		}
